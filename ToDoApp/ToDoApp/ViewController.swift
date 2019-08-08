772556d6a1488bec9b9a14d829b92d3e2fb93f2d
Kirill Ivanov <ivanofff.k@gmail.com>
2019-08-08 19:30:35 +0300

    Initial

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Кирилл Иванов on 08/08/2019.
+//  Copyright © 2019 Kirill Ivanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
